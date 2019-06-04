d74985a693f900a8ce9a0904eaa2c796adbb9884
BrayanRGG <49702228+BrayanRGG@users.noreply.github.com>
2019-06-04 09:44:09 -0500

    Big code, New weights, kinda working

--- Python Code/Test/Test_array.py
+++ Python Code/Test/Test_array.py
@@ -3,6 +3,15 @@ import numpy as np
 xa = np.zeros((2,9),dtype=int)
 y = np.array(([1, 2, 3, 4, 5, 6, 7, 8, 100],[ 10,1,32,4,5,5463,3,654,6]),dtype=int)
 
-maxInColumns = np.amin(y, axis=0)
+xPredicted = np.array(([0.836142,    1,   1,   0.889866,    0.90989]), dtype=float)
+xy = np.zeros((200,5),dtype=float)
+i=0
 
-print (maxInColumns)
+while(i<200):
+	j=0
+	while(j<5):
+		xy[i,j] = xPredicted[j]
+		j=j+1
+	i=i + 1
+
+print (xy)
